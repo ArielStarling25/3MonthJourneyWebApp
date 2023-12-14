25e5fc39a6cfd1ed99aab2c10931a3ad8ac824bb
Ariel Starling <arielstarling25@gmail.com>
2023-12-14 09:17:40 +0800

    commit to newlink

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Greetings from the other side -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/MainPage.css" />
