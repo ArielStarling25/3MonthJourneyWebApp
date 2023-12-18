c83b85c62988d16dabfa0629721f3e3804172542
Ariel Starling <arielstarling25@gmail.com>
2023-12-18 11:58:46 +0800

    added css file linked to ProgramRunner

--- ProgramRunner.php
+++ ProgramRunner.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/ProgramRunner.css" />
     <title>Program Runner</title>
 </head>
 <body>
     <div class="PRHeadDiv">
-        
+        <h1>
+            Program Runner
+        </h1>
     </div>
     <div class="PRBodyDiv">
         <?php
