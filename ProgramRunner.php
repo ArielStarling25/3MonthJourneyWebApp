edfec845764eeb97d603a48b88b88fa58f912ef7
Ariel Starling <arielstarling25@gmail.com>
2023-12-22 11:43:46 +0800

    Added button functionality to select certain scripts to run

--- ProgramRunner.php
+++ ProgramRunner.php
@@ -8,11 +8,25 @@
 </head>
 <body>
     <?php 
+        include 'ConsoleWriter.php';
         //Directory for python files
         $dir = 'Uploads/Python';
         $files = scandir($dir);
         $jsonData = json_encode($files);
         $decodedArray = json_decode($jsonData);
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $iD = $_REQUEST['idKey'];
+            $buttonClicked = $_REQUEST['submit'];
+            if($iD == "callScript"){
+                try{
+                    consoleLog($buttonClicked . " was clicked ");
+                }
+                catch(Exception $eX2){
+                    echo $eX2->getMessage();
+                }
+            }
+        }
     ?>
     <div class="PRHeadDiv">
         <div>
@@ -26,29 +40,33 @@
             <div class="ResultBox">
                 <p>
                     <?php
-                        $python = shell_exec("python Uploads/Python/HelloWorld.py");
-                        $formattedOutput = str_replace("\n", "<br>", $python);
-                        echo $formattedOutput;
+                        if(!empty($buttonClicked)){
+                            $python = shell_exec("python Uploads/Python/$buttonClicked");
+                            $formattedOutput = str_replace("\n", "<br>", $python);
+                            echo $formattedOutput;
+                        }
+                        else{
+                            echo "Select a file to run and display...";
+                        }
                     ?>
                 </p>
             </div>
             <div class="PyList">
                 <ol>
                     <form id='programSub' method='post' action='<?php echo $_SERVER['PHP_SELF']; ?>'>
-                    <?php 
-                        foreach($decodedArray as $file){
-                            if($file == "." || $file == ".."){
-                                //omit the empty strings, only include the named files
+                        <input type="hidden" name="idKey" value="callScript">
+                        <?php 
+                            foreach($decodedArray as $file){
+                                if($file == "." || $file == ".."){
+                                    //omit the empty strings, only include the named files
+                                }
+                                else{
+                                    echo "<li>
+                                            <input name='submit' type='submit' value='$file'>
+                                    </li>";
+                                }
                             }
-                            else{
-                                echo "<li>
-                                        <button value='$file'>
-                                            $file
-                                        </button>
-                                </li>";
-                            }
-                        }
-                    ?>
+                        ?>
                     </form>
                 </ol>
             </div>
---
+++ Uploads/Python/Greetings.py
@@ -0,0 +1,8 @@
+
+print("Hello from Greetings\n")
+
+def myFunc(number):
+    return number * 2
+
+print(myFunc(20), " is a cool number bro \n")
+print("So is the number ", myFunc(5))
