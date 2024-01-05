2aa4d5df885d80b81e4e4f7c4ce9702a9a48f660
Ariel Starling <arielstarling25@gmail.com>
2024-01-05 11:39:01 +0800

    TicTacToe win condition check complete

--- MiniApps/TicTacToe/script.js
+++ MiniApps/TicTacToe/script.js
@@ -3,6 +3,7 @@
 const gridElements = document.querySelectorAll('.grid div');
 const botSwitch = document.getElementsByClassName('switch')[0];
 const promptBox = document.getElementsByClassName('prompt')[0];
+let winCond = '';
 let player1Turn = true;
 let boardOccupancyCount = 0;
 
@@ -21,6 +22,7 @@ for (let i = 0; i < gridElements.length; i++) {
     if (boardOccupancyCount < 9) {
       alternateClick(this);
       checkBoard();
+      checkWin();
     } else {
       document.querySelector('.prompt').textContent = 'Game Over';
     }
@@ -36,18 +38,22 @@ let gridView = [
 let gridViewTemp = ['', '', '', '', '', '', '', '', ''];
 
 function alternateClick(node) {
-  if (node.textContent == 'O' || node.textContent == 'X') {
-    promptBox.textContent = 'You cant do that...';
-  } else {
-    if (player1Turn) {
-      player1Turn = false;
-      node.textContent = 'O';
-      promptBox.textContent = 'Player 2 turn...';
+  if (winCond == '') {
+    if (node.textContent == 'O' || node.textContent == 'X') {
+      promptBox.textContent = 'You cant do that...';
     } else {
-      player1Turn = true;
-      node.textContent = 'X';
-      promptBox.textContent = 'Player 1 turn...';
+      if (player1Turn) {
+        player1Turn = false;
+        node.textContent = 'O';
+        promptBox.textContent = 'Player 2 turn...';
+      } else {
+        player1Turn = true;
+        node.textContent = 'X';
+        promptBox.textContent = 'Player 1 turn...';
+      }
     }
+  } else {
+    promptBox.textContent = 'Player ' + winCond + ' Wins';
   }
 }
 
@@ -73,6 +79,66 @@ function checkBoard() {
 }
 
 checkBoard();
+
+//Win condition, kinda hard ngl
+function checkWin() {
+  let winCondition = false;
+  let winPlayer = '';
+  for (let i = 0; i < gridView.length; i++) {
+    let item = gridView[i][0];
+    if (item != '') {
+      if (item == gridView[i][0 + 1] && item == gridView[i][0 + 2]) {
+        //win
+        winCondition = true;
+        winPlayer = item;
+        break;
+      }
+    }
+  }
+
+  if (!winCondition) {
+    for (let i = 0; i < gridView[0].length; i++) {
+      let item = gridView[0][i];
+      if (item != '') {
+        if (item == gridView[0 + 1][i] && item == gridView[0 + 2][i]) {
+          //win
+          winCondition = true;
+          winPlayer = item;
+          break;
+        }
+      }
+    }
+  }
+
+  if (!winCondition) {
+    let gridItem = gridView[0][0];
+    if (gridItem != '') {
+      if (gridItem == gridView[1][1] && gridItem == gridView[2][2]) {
+        winCondition = true;
+        winPlayer = gridItem;
+      }
+    }
+  }
+
+  if (!winCondition) {
+    let gridItem = gridView[2][0];
+    if (gridItem != '') {
+      if (gridItem == gridView[1][1] && gridItem == gridView[0][2]) {
+        winCondition = true;
+        winPlayer = gridItem;
+      }
+    }
+  }
+
+  console.log('Win condition: ', winCondition, 'Win player: ', winPlayer);
+  if (winCondition) {
+    if (winPlayer == 'O') {
+      winCond = '1';
+    } else if (winPlayer == 'X') {
+      winCond = '2';
+    }
+  }
+}
 
 //Bot Functionality
 function performAI() {}
